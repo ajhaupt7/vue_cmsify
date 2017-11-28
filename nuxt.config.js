1e74e1f9b758c487541b06879f3bcd2d94fbb0f8
Andrew Haupt <ajhaupt7@gmail.com>
2017-11-28 08:26:21 -0800

    Fixes typo in description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Template for building Vue CMS projects w/ Nuxt + Nuxntent + Netlify' }
+      { hid: 'description', name: 'description', content: 'Template for building Vue CMS projects w/ Nuxt + Nuxtent + Netlify' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
