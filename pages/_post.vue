a561ca9643a70937bb14c5c3c1b0f8dce361dfbd
Andrew Haupt <ajhaupt7@gmail.com>
2017-11-28 08:26:26 -0800

    Sets up post page

---
+++ pages/_post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <section v-html="post.body" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ app, route }) {
+    return {
+      post: await app.$content('/').get(route.path)
+    }
+  }
+}
+</script>
