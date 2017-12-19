6b328af0439880a81a10187942f351d113d2fdd2
yasark <yasarkkk@protonmail.com>
2017-12-20 00:28:33 +0100

    added twitter icon and animation

--- components/SocialBar.vue
+++ components/SocialBar.vue
@@ -2,17 +2,20 @@
   <div class='social-bar'>
     <a class='icon' href='https://github.com/yashha'><Github /></a>
     <a class='icon' href='https://github-profile-summary.com/user/yashha'><GithubProfile /></a>
+    <a class='icon' href='https://github-profile-summary.com/user/yashha'><Twitter /></a>
   </div>
 </template>
 
 <script>
 import Github from '~/components/icons/Github'
 import GithubProfile from '~/components/icons/GithubProfile'
+import Twitter from '~/components/icons/Twitter'
 
 export default {
   components: {
     Github,
-    GithubProfile
+    GithubProfile,
+    Twitter
   }
 }
 </script>
@@ -24,6 +27,11 @@ export default {
     .icon {
       display: inline-block;
       margin: 0 30px;
+      transition: all 0.3s ease-out;
+      
+      &:hover {
+        transform: scale(1.3);
+      }
     }
   }
 </style>
